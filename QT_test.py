37928507d896be2088b6121fe0df6a2a3d73159e
VissageMo <434753117@qq.com>
2018-10-31 18:52:20 +0800

    2018.10.31

--- QT_test.py
+++ QT_test.py
@@ -1,26 +1,11 @@
 # -*- coding: utf-8 -*-
 
-"""
-ZetCode PyQt5 tutorial
-
-In this example, we display the x and y
-coordinates of a mouse pointer in a label widget.
-
-Author: Jan Bodnar
-Website: zetcode.com
-Last edited: August 2017
-"""
-
+from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QPushButton,
+                             QSizePolicy, QLabel, QFontDialog, QApplication)
 import sys
-from PyQt5.QtCore import pyqtSignal, QObject
-from PyQt5.QtWidgets import QMainWindow, QApplication
 
 
-class Communicate(QObject):
-    closeApp = pyqtSignal()
-
-
-class Example(QMainWindow):
+class Example(QWidget):
 
     def __init__(self):
         super().__init__()
@@ -28,21 +13,35 @@ class Example(QMainWindow):
         self.initUI()
 
     def initUI(self):
+        vbox = QVBoxLayout()
 
-        self.c = Communicate()
-        self.c.closeApp.connect(self.close)
+        btn = QPushButton('Dialog', self)
+        btn.setSizePolicy(QSizePolicy.Fixed,
+                          QSizePolicy.Fixed)
 
-        self.setGeometry(300, 300, 290, 150)
-        self.setWindowTitle('Email signal')
+        btn.move(20, 20)
+
+        vbox.addWidget(btn)
+
+        btn.clicked.connect(self.showDialog)
+
+        self.lbl = QLabel('Knowledge only matters', self)
+        self.lbl.move(130, 20)
+
+        vbox.addWidget(self.lbl)
+        self.setLayout(vbox)
+
+        self.setGeometry(300, 300, 250, 180)
+        self.setWindowTitle('Font dialog')
         self.show()
 
-    def mousePressEvent(self, event):
-
-        self.c.closeApp.emit()
+    def showDialog(self):
+        font, ok = QFontDialog.getFont()
+        if ok:
+            self.lbl.setFont(font)
 
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
